5d457936521bbb10bf5cc67e62b888f62143ace6
Omosuyi Olawole <omooladev@gmail.com>
2024-04-14 01:29:32 +0100

    fixed the bug responsible for creating error when the page is refreshed

--- src/context/productContext.js
+++ src/context/productContext.js
@@ -6,18 +6,23 @@ export const productContext = createContext(null);
 const getDefaultCart = () => {
   let cart = {};
   for (let i = 1; i < allProducts.length + 1; i++) {
-    cart[i] = 0;
+    //----------> push the all products to the cart
+    cart[i] = allProducts[i];
   }
   return cart;
 };
 
 const retrievedCart = () => {
-  const newItem = localStorage.getItem("cartItems");
-  return newItem ? JSON.parse(newItem) : getDefaultCart;
+  let newItem = localStorage.getItem("cartItems");
+  if (!newItem) {
+    //----------> if no item in the cart
+    newItem = getDefaultCart();
+    return newItem;
+  }
+  return JSON.parse(newItem);
 };
 export const ProductContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(retrievedCart);
-
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }, [cartItems]);
@@ -26,9 +31,7 @@ export const ProductContextProvider = (props) => {
     let totalAmount = 0;
     for (const item in cartItems) {
       if (cartItems[item] > 0) {
-        let itemInfo = allProducts.find(
-          (product) => product.id === Number(item)
-        );
+        let itemInfo = allProducts.find((product) => product.id === Number(item));
         totalAmount += cartItems[item] * itemInfo.Price;
       }
     }
@@ -60,11 +63,7 @@ export const ProductContextProvider = (props) => {
     checkout,
   };
 
-  return (
-    <productContext.Provider value={contextValue}>
-      {props.children}
-    </productContext.Provider>
-  );
+  return <productContext.Provider value={contextValue}>{props.children}</productContext.Provider>;
 };
 
 export default ProductContextProvider;
